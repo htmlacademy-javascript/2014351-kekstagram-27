28d79784b0d2b0e6243e4134a1a650744954c44d
Julia Golovko <ulechka19981998@gmail.com>
2022-11-02 19:41:46 +0300

    сделала рефакторинг модуля слайдера для эффектов

--- js/effects-slider.js
+++ js/effects-slider.js
@@ -1,104 +1,49 @@
+import {EFFECTS_CONFIG} from './config.js';
+
 const uploadForm = document.querySelector('#upload-select-image');
 const effectsSlider = uploadForm.querySelector('.effect-level');
-const sliderValue = effectsSlider.querySelector('.effect-level__value');
+const sliderInput = effectsSlider.querySelector('.effect-level__value');
 const effectsList = uploadForm.querySelector('.effects__list');
 const imgPreview = uploadForm.querySelector('.img-upload__preview').children[0];
 
-const onEffectChange = (evt) => {
-  if (evt.target.value !== 'none') {
-    effectsSlider.classList.remove('hidden');
-  }
+const showEffectsSlider = () => {
+  effectsSlider.classList.remove('hidden');
+};
 
-  switch (evt.target.value) {
-    case 'chrome':
-    case 'sepia':
-      effectsSlider.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 1,
-        },
-        start: 1,
-        step: 0.1,
-      });
-      break;
-    case 'marvin':
-      effectsSlider.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 100,
-        },
-        start: 100,
-        step: 1,
-      });
-      break;
-    case 'phobos':
-      effectsSlider.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 3,
-        },
-        start: 3,
-        step: 0.1,
-      });
-      break;
-    case 'heat':
-      effectsSlider.noUiSlider.updateOptions({
-        range: {
-          min: 1,
-          max: 3,
-        },
-        start: 3,
-        step: 0.1,
-      });
-      break;
-    default:
-      effectsSlider.noUiSlider.updateOptions({
-        range: {
-          min: 0,
-          max: 100,
-        },
-        start: 100,
-        step: 1,
-      });
-      effectsSlider.classList.add('hidden');
+const hideEffectsSlider = () => {
+  effectsSlider.classList.add('hidden');
+};
+
+const onEffectChange = (evt) => {
+  const effect = evt.target.value;
+
+  // EFFECTS_CONFIG.hasOwnProperty(effect)
+  if (Object.prototype.hasOwnProperty.call(EFFECTS_CONFIG, effect)) {
+    effectsSlider.noUiSlider.updateOptions(EFFECTS_CONFIG[effect].slider);
+    showEffectsSlider();
+  } else {
+    effectsSlider.noUiSlider.updateOptions(EFFECTS_CONFIG.default.slider);
+    hideEffectsSlider();
   }
 };
 
 const initSlider = () => {
-  noUiSlider.create(effectsSlider, {
-    range: {
-      min: 0,
-      max: 100,
-    },
-    start: 100,
-    step: 1,
-  });
+  noUiSlider.create(effectsSlider, EFFECTS_CONFIG.default.slider);
 
   effectsSlider.noUiSlider.on('update', () => {
-    sliderValue.value = effectsSlider.noUiSlider.get();
+    const value = effectsSlider.noUiSlider.get();
     const selectedEffect = effectsList.querySelector('[name="effect"]:checked').value;
-    let filter = '';
-    let unit = '';
-    switch (selectedEffect) {
-      case 'chrome':
-        filter = 'grayscale';
-        break;
-      case 'marvin':
-        filter = 'invert';
-        unit = '%';
-        break;
-      case 'sepia':
-        filter = 'sepia';
-        break;
-      case 'phobos':
-        filter = 'blur';
-        unit = 'px';
-        break;
-      case 'heat':
-        filter = 'brightness';
-    }
 
-    imgPreview.style = filter.length ? `filter: ${filter}(${sliderValue.value}${unit})` : '';
+    // EFFECTS_CONFIG.hasOwnProperty(selectedEffect)
+    if (Object.prototype.hasOwnProperty.call(EFFECTS_CONFIG, selectedEffect)) {
+      const {filter, unit} = EFFECTS_CONFIG[selectedEffect];
+
+      imgPreview.style = `filter: ${filter}(${value}${unit})`;
+      sliderInput.value = value;
+    } else {
+      imgPreview.style = '';
+      sliderInput.value = '';
+    }
   });
 
   effectsList.addEventListener('change', onEffectChange);
